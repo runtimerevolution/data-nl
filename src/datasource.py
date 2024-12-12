7241c8a26d910461af24993549621f4064ef3e3d
João Silva <j.silva@runtime-revolution.com>
2024-12-12 14:24:01 +0000

    Remove database settings in favor of database uri

--- src/datasource.py
+++ src/datasource.py
@@ -7,12 +7,6 @@ from llama_index.readers.database import DatabaseReader
 
 READER = DatabaseReader(
     uri=os.getenv("DATABASE_URI", "postgresql://postgres:postgres@db:5432/postgres"),
-    # scheme=os.getenv("DATABASE_SCHEMA", "postgres"),
-    # host=os.getenv("DATABASE_HOST", "localhost"),
-    # port=os.getenv("DATABASE_PORT", "5432"),
-    # user=os.getenv("DATABASE_USER", "postgres"),
-    # password=os.getenv("DATABASE_PASSWORD", "postgres"),
-    # dbname=os.getenv("DATABASE_NAME", "postgres"),
 )
 
 
